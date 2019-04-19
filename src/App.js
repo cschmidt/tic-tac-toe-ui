738b4d16e0dda50eac9d2a54046016b40ead18be
Carl Schmidt <carl.schmidt@gmail.com>
2019-04-19 02:00:58 +0000

    Exploring both Cognito and Google auth APIs (need to choose one)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css'
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import AWS from 'aws-sdk'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { startGame, submitMove } from './actions/tic-tac-toe-actions'
 import Board from './components/board'
 import Home from './components/home'
 import Login from './components/login'
 import TopNav from './components/top-nav'
-
-
-AWS.config.region = 'us-west-2'
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: 'us-west-2:4c116aed-72e0-4fb1-86ed-76f9ea375aef'
-})
 
 
 const BoardContainer = connect(
@@ -31,7 +24,6 @@ const BoardContainer = connect(
     }
   }
 )(Board)
-
 
 
 const App = () => (
